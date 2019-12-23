a3af7b75df5d35d9b9b888f07cfd4d2889f96f0b
Sitansu Subudhi <sitansu.subudhi@outlook.com>
2019-12-24 00:51:12 +0530

    feat: Connect Login component to store

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,71 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Dropdown, Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import { connect } from 'react-redux';
 
-const friendOptions = [
-    {
-        key: 'Jenny Hess',
-        text: 'Jenny Hess',
-        value: 'Jenny Hess',
-        image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/jenny.jpg' },
-    },
-    {
-        key: 'Elliot Fu',
-        text: 'Elliot Fu',
-        value: 'Elliot Fu',
-        image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg' },
-    },
-    {
-        key: 'Stevie Feliciano',
-        text: 'Stevie Feliciano',
-        value: 'Stevie Feliciano',
-        image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/stevie.jpg' },
-    },
-    {
-        key: 'Christian',
-        text: 'Christian',
-        value: 'Christian',
-        image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/christian.jpg' },
-    },
-    {
-        key: 'Matt',
-        text: 'Matt',
-        value: 'Matt',
-        image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/matt.jpg' },
-    },
-    {
-        key: 'Justen Kitsune',
-        text: 'Justen Kitsune',
-        value: 'Justen Kitsune',
-        image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/justen.jpg' },
-    },
-]
 
-const Login = () => (
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 500 }}>
-            <Header as='h2' color='teal' textAlign='center'>
-                <Message size='small'>
-                    Welcome to the Would You Rather App
-                </Message>
-            </Header>
-            <Form size='large'>
-                <Segment >
-                    <Form.Field>
-                        <Dropdown
-                            placeholder='Select user to log in'
-                            fluid
-                            selection
-                            options={friendOptions}
-                        />
-                    </Form.Field>
-                    <Button color='teal' fluid size='large'>
-                        Login
-                    </Button>
-                </Segment>
-            </Form>
-        </Grid.Column>
-    </Grid>
-)
+class Login extends Component {
 
-export default Login
+    render() {
+        const { users } = this.props;
+        const userOptions = Object.keys(users).map(userID => {
+            const user = users[userID];
+
+            return {
+                key: user.id,
+                text: user.name,
+                value: user.id,
+                image: {avatar: true, src: user.avatarURL}
+            };
+        });
+        const friendOptions = [
+            {
+                key: 'Jenny Hess',
+                text: 'Jenny Hess',
+                value: 'Jenny Hess',
+                image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/jenny.jpg' },
+            },
+            {
+                key: 'Elliot Fu',
+                text: 'Elliot Fu',
+                value: 'Elliot Fu',
+                image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/elliot.jpg' },
+            },
+            {
+                key: 'Stevie Feliciano',
+                text: 'Stevie Feliciano',
+                value: 'Stevie Feliciano',
+                image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/stevie.jpg' },
+            },
+            {
+                key: 'Christian',
+                text: 'Christian',
+                value: 'Christian',
+                image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/christian.jpg' },
+            },
+            {
+                key: 'Matt',
+                text: 'Matt',
+                value: 'Matt',
+                image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/matt.jpg' },
+            },
+            {
+                key: 'Justen Kitsune',
+                text: 'Justen Kitsune',
+                value: 'Justen Kitsune',
+                image: { avatar: true, src: 'https://react.semantic-ui.com/images/avatar/small/justen.jpg' },
+            },
+        ];
+
+
+        return (
+            <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth: 500 }}>
+                    <Header as='h2' color='teal' textAlign='center'>
+                        <Message size='small'>
+                            Welcome to the Would You Rather App
+                    </Message>
+                    </Header>
+                    <Form size='large'>
+                        <Segment >
+                            <Form.Field>
+                                <Dropdown
+                                    placeholder='Select user to log in'
+                                    fluid
+                                    selection
+                                    options={userOptions}
+                                />
+                            </Form.Field>
+                            <Button color='teal' fluid size='large'>
+                                Login
+                            </Button>
+                        </Segment>
+                    </Form>
+                </Grid.Column>
+            </Grid>
+        );
+    }
+}
+
+function mapStateToProps({ users }) {
+    return {
+        users
+    }
+}
+
+export default connect(mapStateToProps)(Login)
--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: '/images/karen.jpg', // todo: add avatar image
+    avatarURL: '/images/IMG_1031_LinkedIn.jpg',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: '/images/tyler.jpg', // todo: add avatar image
+    avatarURL: '/images/tyler.jpg', 
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: '/images/richard.jpg', // todo: add avatar image
+    avatarURL: '/images/20171230_170339_account.jpg',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
