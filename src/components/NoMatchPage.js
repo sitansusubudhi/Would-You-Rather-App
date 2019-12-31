19dc2678d29f62cdd3da5fb6baab3322b572c0fa
Sitansu Subudhi <sitansu.subudhi@outlook.com>
2020-01-01 01:13:14 +0530

    feat: Add Redirect option in 404 page

--- src/components/NoMatchPage.js
+++ src/components/NoMatchPage.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Header, Icon, Segment, Button } from 'semantic-ui-react';
+import { withRouter } from 'react-router-dom';
 
-const NoMatchPage = () => (
-    <Segment placeholder>
-        <Header as='h2' icon>
-            <Icon name='settings' />
-            404 - Not Found
-            <Header.Subheader>
-                The page link seems to be broken. Try to log in again.
-            </Header.Subheader>
-        </Header>
-        <Button color='black'>Login</Button>
-    </Segment>
+class NoMatchPage extends Component {
 
-);
+    handlePageNoMatch = e => {
+        e.preventDefault();
+        this.props.history.push('/');
+    };
 
-export default NoMatchPage;
+    render() {
+        return (
+            <Segment placeholder>
+                <Header as='h2' icon>
+                    <Icon name='settings' />
+                    404 - Not Found
+                    <Header.Subheader>
+                        The page link seems to be broken. Try to log in again.
+                    </Header.Subheader>
+                </Header>
+                <Button
+                    color='black'
+                    onClick={this.handlePageNoMatch}>
+                    Go Home
+                </Button>
+            </Segment>
+        );
+    }
+}
+
+export default withRouter(NoMatchPage);
