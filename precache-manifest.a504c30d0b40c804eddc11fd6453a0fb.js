794712fb6770302b3220780b0c967ef39eecce98
Sitansu Subudhi <sitansu.subudhi@outlook.com>
2020-03-16 21:13:35 +0530

    Updates

---
+++ precache-manifest.a504c30d0b40c804eddc11fd6453a0fb.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7811e064a7468e10868719baa2fb67",
+    "url": "/would-you-rather-app/index.html"
+  },
+  {
+    "revision": "afee9b20f9b50b77c6a1",
+    "url": "/would-you-rather-app/static/css/2.596083c7.chunk.css"
+  },
+  {
+    "revision": "5225b3487ecb20901dde",
+    "url": "/would-you-rather-app/static/css/main.81813dda.chunk.css"
+  },
+  {
+    "revision": "afee9b20f9b50b77c6a1",
+    "url": "/would-you-rather-app/static/js/2.8c52a779.chunk.js"
+  },
+  {
+    "revision": "f8513fb9f2981a1b206fb733b985f272",
+    "url": "/would-you-rather-app/static/js/2.8c52a779.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5225b3487ecb20901dde",
+    "url": "/would-you-rather-app/static/js/main.dd3a23ec.chunk.js"
+  },
+  {
+    "revision": "c4ded327872556ff56c2",
+    "url": "/would-you-rather-app/static/js/runtime-main.3c8cd565.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/would-you-rather-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/would-you-rather-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/would-you-rather-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/would-you-rather-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/would-you-rather-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/would-you-rather-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/would-you-rather-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/would-you-rather-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/would-you-rather-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/would-you-rather-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/would-you-rather-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/would-you-rather-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/would-you-rather-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/would-you-rather-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/would-you-rather-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/would-you-rather-app/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
